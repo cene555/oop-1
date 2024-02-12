c1a8611d9457d333d0e4a33b7d39018d2a2d6771
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2024-02-12 19:51:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include "rectangle.hpp"
 #include <cmath>
 
+Rectangle::Rectangle(double newX1, double newY1, double newX2, double newY2){
+        x1 = newX1;
+        y1 = newY1;
+        x2 = newX2;
+        y2 = newY2;
+    }
+
+double Rectangle::perimeter(){
+    return abs(x1 - x2) * 2 + abs(y1 - y2) * 2;
+}
+
+double Rectangle::square(){
+        return abs(x1 - x2) * abs(y1 - y2);
+} 
+
 int main(){
     Rectangle circle1;
     std::cout << "perimeter: " << circle1.perimeter() << " square: " << circle1.square() << std::endl;
